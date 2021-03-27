49d039779dcff2e60325dd4af7b345b4ad7ee047
rodriguesrenato <renato.rodrigues.rfr@gmail.com>
2021-03-26 23:58:35 -0300

    feat:Added StrClean method

--- include/format.h
+++ include/format.h
@@ -5,6 +5,7 @@
 
 namespace Format {
 std::string ElapsedTime(long times);  // TODO: See src/format.cpp
+std::string StrClean(std::string, unsigned int length);
 };                                    // namespace Format
 
 #endif
--- src/format.cpp
+++ src/format.cpp
@@ -4,17 +4,38 @@
 #include <string>
 using std::string;
 
-// Format uptime in seconds to a string of HH:MM:SS, 
+// Format upTime in seconds to a string of HH:MM:SS,
 // with hours limited to 4 digits
-string Format::ElapsedTime(long uptime) {
-  int seconds = uptime % 60;
-  int minutes = uptime / 60 % 60;
-  int hours = uptime / 60 / 60 % 60;
+string Format::ElapsedTime(long upTime)
+{
+    int seconds = upTime % 60;
+    int minutes = upTime / 60 % 60;
+    int hours = upTime / 60 / 60 % 60;
 
-  // Max of 4 digits for hours
-  char buffer[10];
-  std::snprintf(buffer, sizeof(buffer), "%02u:%02u:%02u", hours, minutes,
-                seconds);
+    // Max of 4 digits for hours
+    char buffer[10];
+    std::snprintf(buffer, sizeof(buffer), "%02u:%02u:%02u", hours, minutes, seconds);
 
-  return buffer;
+    return buffer;
+}
+
+string Format::StrClean(string str, unsigned int length)
+{
+    string formattedStr;
+
+    if (str.length() > length)
+    {
+        formattedStr = str.substr(0, length - 1);
+    }
+    else
+    {
+        formattedStr = str;
+    }
+
+    for (unsigned int i = str.length(); i < length; i++)
+    {
+        formattedStr += " ";
+    }
+
+    return formattedStr;
 }
