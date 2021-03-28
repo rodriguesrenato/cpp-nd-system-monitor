692625e754fd39533663aeaad2c87dbd652aabaa
rodriguesrenato <renato.rodrigues.rfr@gmail.com>
2021-03-28 04:14:49 -0300

    chore: Added comments

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,8 @@
 #include "ncurses_display.h"
 #include "system.h"
 
+// Initialize system class and display it using NCurseDisplay
 int main() {
-  System system;
-  NCursesDisplay::Display(system);
+    System system;
+    NCursesDisplay::Display(system);
 }
