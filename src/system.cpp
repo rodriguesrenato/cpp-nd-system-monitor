06d906e4c0d616e11fde1602ad1865d1ecf7f971
rodriguesrenato <renato.rodrigues.rfr@gmail.com>
2021-03-28 04:28:14 -0300

    feat:Implemented Process() method

--- src/system.cpp
+++ src/system.cpp
@@ -17,28 +17,69 @@ using std::string;
 using std::vector;
 
 #include <iostream>
-// TODO: Return the system's CPU
-Processor& System::Cpu() { 
-    // cpu_ = Processor();
-    return cpu_; }
 
-// TODO: Return a container composed of the system's processes
-vector<Process>& System::Processes() { return processes_; }
+// Return the system's CPU
+Processor &System::Cpu() { return cpu_; }
 
-// ##DONE: Return the system's kernel identifier (string)
+// Return a container composed of the system's processes
+vector<Process> &System::Processes() {
+    // Get all system pids available now
+    vector<int> pidsUpdated{LinuxParser::Pids()};
+
+    // cached pids
+    vector<int> pidsCached{};
+
+    // loop through all cached process, from end to begin of the vector. Remove
+    // processes that don't exist anymore
+    for (int i = processes_.size() - 1; i >= 0; i--) {
+        // If cached process pid is not in the updated pids, then remove it from
+        // processes_ vector
+        if (std::find(pidsUpdated.begin(), pidsUpdated.end(),
+                      processes_[i].Pid()) == pidsUpdated.end()) {
+            processes_.erase(processes_.begin() + i);
+        }
+        // otherwise add its pid to pidsCached vector
+        else {
+            pidsCached.push_back(processes_[i].Pid());
+        }
+    }
+
+    // Add all updated processes that isnt in the processes_ vector
+    for (int pid : pidsUpdated) {
+        // if pid not found in pidsCached, then add a Process with this pid to
+        // processes_ vector
+        if (std::find(pidsCached.begin(), pidsCached.end(), pid) ==
+            pidsCached.end()) {
+            processes_.emplace_back(pid);
+        }
+    }
+
+    // Update cpu utilization of all process in processes_
+    for (Process &process : processes_) {
+        process.CpuUtilization(LinuxParser::ActiveJiffies(process.Pid()),
+                               LinuxParser::Jiffies());
+    }
+
+    // Sort processes by cpu utilization
+    std::sort(processes_.begin(), processes_.end(), std::greater<Process>());
+
+    return processes_;
+}
+
+// Return the system's kernel identifier (string)
 std::string System::Kernel() { return LinuxParser::Kernel(); }
 
-// ##DONE: Return the system's memory utilization
+// Return the system's memory utilization
 float System::MemoryUtilization() { return LinuxParser::MemoryUtilization(); }
 
-// ##DONE: Return the operating system name
+// Return the operating system name
 std::string System::OperatingSystem() { return LinuxParser::OperatingSystem(); }
 
-// ##DONE: Return the number of processes actively running on the system
+// Return the number of processes actively running on the system
 int System::RunningProcesses() { return LinuxParser::RunningProcesses(); }
 
-// ##DONE: Return the total number of processes on the system
+// Return the total number of processes on the system
 int System::TotalProcesses() { return LinuxParser::TotalProcesses(); }
 
-// ##DONE: Return the number of seconds since the system started running
+// Return the number of seconds since the system started running
 long System::UpTime() { return LinuxParser::UpTime(); }
