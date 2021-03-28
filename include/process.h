caa362d64eb6c7ec83d60c2c5491e1a2416a4d46
rodriguesrenato <renato.rodrigues.rfr@gmail.com>
2021-03-28 04:12:27 -0300

    feat: Added CpuUtilization functions

--- include/process.h
+++ include/process.h
@@ -6,22 +6,23 @@
 Basic class for Process representation
 It contains relevant attributes as shown below
 */
-class Process
-{
-  public:
-    Process():a_(0.16){}
-    int Pid();                              // TODO: See src/process.cpp
-    std::string User();                     // TODO: See src/process.cpp
-    std::string Command();                  // TODO: See src/process.cpp
-    float CpuUtilization();                 // TODO: See src/process.cpp
-    // void CpuUtilization(int activeJiffies,int idleJiffies);
-    std::string Ram();                      // TODO: See src/process.cpp
-    long int UpTime();                      // TODO: See src/process.cpp
-    bool operator<(Process const &a) const; // TODO: See src/process.cpp
+class Process {
+   public:
+    Process(int const pid) : pid_(pid) {}
+    int Pid() const;
+    std::string User();
+    std::string Command();
+    float CpuUtilization() const;
+    void CpuUtilization(long activeJiffies, long totalJiffies);
+    std::string Ram();
+    long int UpTime();
+    bool operator>(Process const &a) const;
 
-    // TODO: Declare any necessary private members
-  private:
-    float a_{0.13};
+   private:
+    int pid_;
+    float cpuUtilization_{0.0};
+    long totalJiffiesPrev_{0};
+    long activeJiffiesPrev_{0};
 };
 
 #endif
--- src/process.cpp
+++ src/process.cpp
@@ -1,62 +1,52 @@
-#include <cctype>
-#include <sstream>
-#include <string>
-#include <unistd.h>
-#include <vector>
 #include "process.h"
 
+#include <cctype>
+#include <string>
+
+#include "linux_parser.h"
+
 using std::string;
-using std::to_string;
-using std::vector;
 
-#include<iostream>
-using std::cout;
-// TODO: Return this process's ID
-int Process::Pid()
-{
-    return 0;
+// Return this process's ID
+int Process::Pid() const { return Process::pid_; }
+
+// Return this process's CPU utilization
+float Process::CpuUtilization() const { return Process::cpuUtilization_; }
+
+// Calculate this process's CPU utilization
+void Process::CpuUtilization(long activeJiffies, long totalJiffies) {
+    float cpuUtilization{0};
+
+    // Only update cached values and calculate cpu utilization if both total and
+    // active Jiffies passed are valid
+    if (totalJiffies > 0 && activeJiffies > 0) {
+        // calculate the difference from current to previous value
+        long activeDiff = activeJiffies - Process::activeJiffiesPrev_;
+        long totalDiff = totalJiffies - Process::totalJiffiesPrev_;
+
+        // Update previous values stored in class
+        Process::activeJiffiesPrev_ = activeJiffies;
+        Process::totalJiffiesPrev_ = totalJiffies;
+
+        cpuUtilization = (float)activeDiff / (float)totalDiff;
+        // calculate cpu utilization and update cpuUtilization_
+    }
+    Process::cpuUtilization_ = cpuUtilization;
 }
 
-// TODO: Return this process's CPU utilization
-float Process::CpuUtilization()
-{
-    // cout << "test: ";
-    // float b = Process::a_ +0.2;
-    // cout << Process::a_;
-    // return b;
-    return 0.0;
-}
+// Return the command that generated this process
+string Process::Command() { return LinuxParser::Command(Process::pid_); }
 
-// void Process::CpuUtilization(int activeJiffies,int idleJiffies) { CpuUtilization_ =
-// (float)activeJiffies/(float)idleJiffies;}
+// Return this process's memory utilization
+string Process::Ram() { return LinuxParser::Ram(Process::pid_); }
 
-// TODO: Return the command that generated this process
-string Process::Command()
-{
-    return string();
-}
+// Return the user (name) that generated this process
+string Process::User() { return LinuxParser::User(Process::pid_); }
 
-// TODO: Return this process's memory utilization
-string Process::Ram()
-{
-    return string();
-}
+// Return the age of this process (in seconds)
+long int Process::UpTime() { return LinuxParser::UpTime(Process::pid_); }
 
-// TODO: Return the user (name) that generated this process
-string Process::User()
-{
-    return string();
-}
-
-// TODO: Return the age of this process (in seconds)
-long int Process::UpTime()
-{
-    return 0;
-}
-
-// TODO: Overload the "less than" comparison operator for Process objects
-// REMOVE: [[maybe_unused]] once you define the function
-bool Process::operator<(Process const &a [[maybe_unused]]) const
-{
-    return true;
+// Operator "less than" is overloaded to compare cpuUtilization_ value
+bool Process::operator>(Process const &a) const {
+    return Process::cpuUtilization_ > a.cpuUtilization_;
 }
